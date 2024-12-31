ca08ce1242b976a25b4fba7515144e224d072ef2
D32SSV <131158288+D32SSV@users.noreply.github.com>
2024-12-31 22:27:15 +0530

    complete till lesson 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import Greet from "./components/Greet";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
+  const personName = {
+    firstName: "Bruce",
+    lastName: "Banner",
+  };
+  const list = [
+    {
+      firstName: "Tony",
+      lastName: "Stark",
+    },
+    {
+      firstName: "Steve",
+      lastName: "Rogers",
+    },
+    {
+      firstName: "Katrina",
+      lastName: "Kaif",
+    },
+  ];
   return (
     <>
-      <p>Click on the Vite and React logos to learn more</p>
+      <Greet name={"Bhai"} msgCount={10} auth={!true} />
+      <Person name={personName} />
+      <PersonList name={list} />
     </>
   );
 }
